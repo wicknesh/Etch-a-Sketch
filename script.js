4a5701bf999da6d7c1434a1fd7a910a7d80a263a
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-03-08 21:10:44 +0530

    Added new features Added opacity returning function for shading style Added button creation script Added opacity style in event function Added new grid creation

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const mainContainer = document.createElement('div');
 mainContainer.setAttribute('id', 'mainContainer');
 
+const btn = document.createElement('button');
+btn.innerText = "New Grid";
+btn.style.fontSize = '25px';
+document.body.append(btn);
+
 for(let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
@@ -11,11 +16,44 @@ document.body.append(mainContainer);
 
 const hoverSquares = document.querySelectorAll('.square');
 
-function hovering(hoverSquares) {
-    hoverSquares.forEach(square => {
-        square.addEventListener('mousemove', () =>
-        square.style.backgroundColor = `rgb(0,0,0)`)
-    })
+hovering(hoverSquares);
+
+let opa = 0;
+function addOpacity(){
+    return opa+=0.2;
 }
 
-hovering(hoverSquares);
+function hovering(hoverSquares) {
+    hoverSquares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.background = 'black';
+            square.style.opacity = addOpacity();
+        });
+    });
+}
+
+btn.addEventListener('click', () => {
+    const gridVal = Number(prompt("Enter grid size: "));
+
+    const width = 700;
+    const height = 500;
+    if(gridVal <= 100) {
+        document.querySelector('#mainContainer').remove();
+        const mainContainer = document.createElement('div');
+        mainContainer.setAttribute('id', 'mainContainer');
+        for(let i = 0; i < gridVal*gridVal; i++){
+            const square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            mainContainer.append(square);
+            square.style.width = width / gridVal+"px";
+            square.style.height = height / gridVal+"px";
+            square.style.boxSizing = 'border-box';
+            square.style.border = '1px black solid';
+            square.style.flexGrow = 1;
+        }
+        opa = 0;
+        document.body.append(mainContainer);
+    }
+    const hoverSquares = document.querySelectorAll('.square');
+    hovering(hoverSquares);
+});
